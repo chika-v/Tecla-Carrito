cd7876ebb9faba9f42e1026d288fffb0ea9f9dd1
Miguel Ángel Rodríguez González <69826204+mikeangel20@users.noreply.github.com>
2021-08-30 18:11:28 -0500

    ENV CONFIG
    Variables de entorno

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 export const env = {
+  //CONFIG SERVER
+  NODE_ENV: process.env.NODE_ENV || "development",
+  HOST: process.env.HOST || "127.0.0.1",
+  PORT: process.env.PORT || 3000,
+  //CREDENCIALES DE ML
   CLIENT_ID: 8220378401914631,
-  CLIENT_SECRET: 'DqfoC9y92QXa004fAVpsI2v7zzoDYHCD',
+  CLIENT_SECRET: "DqfoC9y92QXa004fAVpsI2v7zzoDYHCD",
   REDIRECT_URI: "",
   SYS_PWD: "",
-  //
+  //LINK FIJOS DE ML
   CATEGORIES_ALL: "https://api.mercadolibre.com/sites/MLM/categories",
   FILTER_CATEGORIES: "https://api.mercadolibre.com/sites/MLM/search?category=",
-  FILTER_PRODUCT_NAME: "https://api.mercadolibre.com/sites/MLM/search?q=Motorola%20G6",
-}
+  FILTER_PRODUCT_NAME:
+    "https://api.mercadolibre.com/sites/MLM/search?q=Motorola%20G6",
+};
